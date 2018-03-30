31c4717a3dc189a1d3d170d062eaf08534e95d0f
Victor P <brukazyabr@gmail.com>
2018-03-30 17:22:56 +0300

    Exercise2

---
+++ l2/ex2/script.js
@@ -0,0 +1,38 @@
+
+let budget = +prompt("Какой ваш бюджет на месяц в рублях?");
+let nameMag = prompt("Какое имя вашего магазина?");
+let time = new Date();
+let hours = time.getHours();
+
+mainList  = {
+	budget,
+	nameMag,
+	open: false,   
+	shopGoods : [],
+	employers : {
+		/*name,
+		age,
+		IsMarried: true*/
+	}
+}
+
+for (let i = 0; i < 3; i++) {
+	let nameThing = prompt("Какой тип товара будем продавать?");
+	if((typeof(nameThing)) === "string" && (typeof(nameThing)) !== null && nameThing.length < 50 && nameThing != "") {
+		mainList.shopGoods.push(nameThing);
+	} else {
+		alert("Введите тип товаров правильно!");
+	}
+	
+}
+
+if(hours > 8 && hours < 20) {
+	mainList.open = true;
+	alert("Магазин открыт!");
+} else {
+	alert("Приходите с 8:00 до 20:00 по Московскому времени!")
+}
+
+alert("Ваш ежедневный бюджет: " + budget/30 + " рублей.");
+
+console.log(mainList.shopGoods);
