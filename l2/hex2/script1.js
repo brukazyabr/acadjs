5b2e5bfd3429e02a1a7c83d54c189212470b14c4
Victor P <brukazyabr@gmail.com>
2018-03-30 20:49:14 +0300

    HardExercise2

--- l2/hex2/script1.js
+++ l2/hex2/script1.js
@@ -4,11 +4,11 @@ var i = day.getUTCDay();
 var today = week[i - 1];
 for (var m = 0; m < 5; m++) {
 	if (m == (i - 1)) {
-		alert(today.italics());
+		document.write(today.italics() + "\n");
 	} else {
-		alert(week[m]);
+		document.write(week[m] + "\n");
 	}
 }
 for (n = 5; n < 7; n++) {
-	alert(week[n].bold());
+	document.write(week[n].bold() + "\n");
 }
