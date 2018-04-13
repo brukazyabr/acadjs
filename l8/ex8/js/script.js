96f85a46f89fb0c9ca5c078a02c114ba433cb8ed
Victor P <brukazyabr@gmail.com>
2018-04-13 05:16:11 +0300

    Exercise8

--- l8/ex8/js/script.js
+++ l8/ex8/js/script.js
@@ -37,10 +37,18 @@ window.addEventListener('DOMContentLoaded', () => {
 	let deadLine = '2018-03-20';
 
 	function getTimeRemainig(endTime) {
-		let t = Date.parse(endTime) - Date.parse(new Date()),
-			seconds = Math.floor((t/1000)&60),
+		let t;
+		if (Date.parse(endTime) > Date.parse(new Date())) {
+			t = Date.parse(endTime) - Date.parse(new Date());
+			seconds = Math.floor((t/1000)&60),		
 			minutes = Math.floor((t/1000/60)&60),
 			hours = Math.floor(t/(1000*3600));
+		} else {
+			seconds = "00";
+			minutes = "00";
+			hours = "00";
+		}
+		
 
 		return {
 			'total': t,
